51bce9439e6946b9d40f7ea27a9da21b52b24f96
14301055 <851005226@qq.com>
2016-12-31 01:41:39 +0800

    kkk

--- service/serviceImpl/MarkServiceImpl.java
+++ service/serviceImpl/MarkServiceImpl.java
@@ -2,158 +2,70 @@ package markpicture;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.List;
 
 import javax.imageio.ImageIO;
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
-import cn.edu.bjtu.weibo.dao.ImgDAO;
-import cn.edu.bjtu.weibo.service.MultipartFile;
-import cn.edu.bjtu.weibo.service.Picture;
-import cn.edu.bjtu.weibo.service.PictureService;
-
-public final class MarkServiceImpl implements MarkService,ImgDAO,PictureService{
- public MarkServiceImpl() {
-	 
- }
-public static void MARK_TEXT(String MARK_TEXT, String imgOrUrl, String ImgThurl, int x, int y)
-{
-   try {
-    File upload = new File(imgOrUrl);
-    Image src = ImageIO.read(upload);
-   int wideth = src.getWidth(null );
-   int height = src.getHeight(null);
-   BufferedImage image = new BufferedImage(wideth, height,
-    BufferedImage.TYPE_INT_RGB);
-    Graphics g = image.createGraphics();
-    g.drawImage(src, 0, 0, wideth, height, null);
-
-   g.setColor(Color.BLACK);
-   g.setFont(new Font(FONT_NAME, FONT_STYLE, FONT_SIZE));
-   g.drawString(MARK_TEXT, wideth - FONT_SIZE - x, height - FONT_SIZE
-		   / 2 - y);
-   g.dispose();
-   FileOutputStream out = new FileOutputStream(ImgThurl);
-   JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
-   encoder.encode(image);
-   out.close();
-   } catch (Exception e) {
-	   System.out.println(e);   
-}
-}
-@Override
-public String watermark(File upload, String imageid, String uploadPath, String imgOrUrl) {
-	   InputStream is = null;
-	   OutputStream os = null;
-	     try{
-	       is=new FileInputStream(upload);
-	   	   os=new FileOutputStream(imgOrUrl+"/"+imageid);
-	   	byte[] buffer=new byte[1024];
-	   	int len=0;
-	   	
-	   	while((len=is.read())>0){
-	   		os.write(buffer);
-	   	}
-	   	
-	   }catch(Exception e){
-	   	e.printStackTrace();
-	   }finally{
-	   	if(is!=null){
-	   		try{
-	   			is.close();
-	   		}catch(Exception e2){
-	   			e2.printStackTrace();
-	   		}
-	   	}
-	   	if(os!=null){
-	   		try{
-	   			os.close();
-	   		}catch(Exception e2){
-	   			e2.printStackTrace();
-	   		}
-	   	}
-	   }
-
-	   return imgOrUrl+"/"+imageid;
-}
-@Override
-public String insert(String time, String imgOrUrl, String ImgThurl) {
-	// TODO Auto-generated method stub
-	return null;
-}
-@Override
-public String getimgOrUrl(String imgid) {
-	return null;
-}
-
-@Override
-public String getimgThUrl(String imgid) {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public String getLikeNumber(String imgid) {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public List<String> getLikeList(String imgid) {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public boolean updateLikeNumber(String imgid) {
-	// TODO Auto-generated method stub
-	return false;
-}
-
-@SuppressWarnings("restriction")
-@Override
-public boolean updateLikeList(String imgid) {
-	// TODO Auto-generated method stub
-	return false;
-}
-
-@Override
-public boolean delete(String imgid) {
-	// TODO Auto-generated method stub
-	return false;
-}
-
-@Override
-public boolean updateNumberofimg() {
-	// TODO Auto-generated method stub
-	return false;
-}
-
-@Override
-public String getimgNumber() {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-public Picture uploadPicture(MultipartFile multipartFile) {
-	// TODO Auto-generated method stub
-	return null;
-}
-@Override
-public cn.edu.bjtu.weibo.model.Picture uploadPicture(org.springframework.web.multipart.MultipartFile multipartFile) {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-
-
+/**
+ * 
+ * @use java给图片添加水印文字
+ */
+public class MarkServiceImpl {
+	/**
+	 * 图片添加水印
+	 * @param srcImgPath 需要添加水印的图片的路径
+	 * @param outImgPath 添加水印后图片输出路径
+	 * @param markContentColor 水印文字的颜色
+	 * @param waterMarkContent 水印的文字
+	 */
+	public void mark(String srcImgPath, String outImgPath, Color markContentColor, String waterMarkContent) {
+		try {
+            // 读取原图片信息
+            File upload = new File(srcImgPath);
+            Image srcImg = ImageIO.read(upload);
+            int srcImgWidth = srcImg.getWidth(null);
+            int srcImgHeight = srcImg.getHeight(null);
+            // 加水印
+            BufferedImage bufImg = new BufferedImage(srcImgWidth, srcImgHeight, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = bufImg.createGraphics();
+            g.drawImage(srcImg, 0, 0, srcImgWidth, srcImgHeight, null);
+            //Font font = new Font("Courier New", Font.PLAIN, 12);
+            Font font = new Font("微软雅黑", Font.PLAIN, 50);  
+            g.setColor(markContentColor); //根据图片的背景设置水印颜色
+            
+            g.setFont(font);
+            int x = srcImgWidth - getWatermarkLength(waterMarkContent, g) - 3;
+            int y = srcImgHeight - 3;
+            //int x = (srcImgWidth - getWatermarkLength(watermarkStr, g)) / 2;
+            //int y = srcImgHeight / 2;
+            g.drawString(waterMarkContent, x, y);
+            g.dispose();
+            // 输出图片
+            FileOutputStream outImgStream = new FileOutputStream(outImgPath);
+            ImageIO.write(bufImg, "jpg", outImgStream);
+            outImgStream.flush();
+            outImgStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	/**
+	 * 获取水印文字总长度
+	 * @param waterMarkContent 水印的文字
+	 * @param g
+	 * @return 水印文字总长度
+	 */
+    public int getWatermarkLength(String waterMarkContent, Graphics2D g) {
+        return g.getFontMetrics(g.getFont()).charsWidth(waterMarkContent.toCharArray(), 0, waterMarkContent.length());
+    }
+    
+    public static void main(String[] args) {
+    	// 原图位置, 输出图片位置, 水印文字颜色, 水印文字
+        new MarkServiceImpl().mark("srcImgPath", "srcImgPath", Color.WHITE, "@爪哇微博通用水印OVO");
+   }
 }
